819d86cdd2dbae2cc0a09a9e0e073efd5c538155
Abhishek Paul <abhishekpaul04022006@gmail.com>
2025-09-19 21:23:19 +0530

    Added INfosec Install Section

--- Win11_Ricer.ps1
+++ Win11_Ricer.ps1
@@ -10,7 +10,7 @@ function Uninstall-App{
             Write-Warning "Failed to Uninstall App: $($_.Exception.Message)"
         }
     }
-    Read-Host -Prompt "Files Deleted Press Enter for Proceeding: "
+    Read-Host -Prompt "Apps Deleted Press Enter for Proceeding: "
 }
 function Install-App{
     $applist= Get-Content -Path "./install_applist.txt"
@@ -23,7 +23,21 @@ function Install-App{
             Write-Warning "Failed to Install App: $($_.Exception.Message)"
         }
     }
-    Read-Host -Prompt "Files Installed Press Enter for Proceeding: "
+    Read-Host -Prompt "Apps Installed Press Enter for Proceeding: "
+}
+function Install-Infosec-Suite-App{
+    $applist= Get-Content -Path "./infosec_suite.txt"
+    foreach($app in $applist){
+        try{
+            winget install --id $app -e --accept-package-agreements --accept-source-agreements
+            Write-Host "$app Installed"
+        }
+        catch{
+            Write-Warning "Failed to Install App: $($_.Exception.Message)"
+        }
+    }
+    Read-Host -Prompt "Apps Installed Press Enter for Proceeding: "
 }
 Uninstall-App
 Install-App
+Install-Infosec-Suite-App
